32ff74f64fcc3e100820cb873295acf8fb640ab5
TillNicke <33596932+TillNicke@users.noreply.github.com>
2017-11-12 14:26:49 +0100

    Update ReplacerSteadyState.py

--- ReplacerSteadyState.py
+++ ReplacerSteadyState.py
@@ -2,15 +2,15 @@
 import random
 
 def SteadyState(population,children)
-childrensize = len(children)
-populationsize = len(population)
+childrensize = len(children) - 1
+populationsize = len(population) - 1
 
 #select random parents from the population and remove them
 for i in range(0,childrensize):
   kill = random.randint(0:populationsize)
   population.remove(kill)
 
- #Children take place in new population
+#Children take place in new population
 population.appennd(children)
 
 return population
