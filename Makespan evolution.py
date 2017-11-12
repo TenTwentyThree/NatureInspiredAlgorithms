2165e9c44a9000b484ad9e40f662dd36fa558ec0
Johannes Schrumpf <jschrumpf@uos.de>
2017-11-12 12:57:24 +0100

    Updated some describtions for better understanding

--- Makespan evolution.py
+++ Makespan evolution.py
@@ -7,13 +7,17 @@ Created on Sat Nov 11 08:39:05 2017
 
 import random
 
-
+#---------------------------------------------------------------P R E D E F I N I T I O N   O F   E X P E R I M E N T   A N D   O B J E C T S --------------------
 class individual:
-    """an indivdual is defined by its genome and its fitness"""
+    """an indivdual is a python object that has the values "genome" and "fitness"
+    The genome is a list of integer between 1 and 20, encoding job distribution. Its length is determined by the number of jobs
+    fitness is the eucledian distance between all total machine runtimes. If the distance is low, the fitness is high."""
     def __init__(self, genome, fitness):
         self.genome = genome
         self.fitness = fitness
-        
+        """the fitness of an individual is updated with simply calling individual.update_fitness()"""
+    def update_fitness()
+
         
 def gen_rand_standard(nmbofitems):
     """This generator generates jobs for experiment 1&2"""
@@ -33,11 +37,15 @@ def gen_rand_two(nmbofitems):
         newjob = random.randint(400,700)
         yield newjob
 
+#---------------------------------------------------------------I M P L E M E N T A T I O N   O F   G E N E T I C   A L G O R I T H M -------------------------------------
         
-        
-def generate_population_from_genomes(listofgenomes):
+def generate_population_from_genes(listofgenomes):
     """This function generates individuals from the list of genomes that is passed into it. it returns a list of individuals which is the new population.
-    This function can be used universally across all experiments"""
+    This function can be used universally across all experiments
+    
+    INPUT: A list of chromosoms
+    
+    OUTPUT: A list of individuals (population)"""
     population = []
     indexofgenomes = len(listofgenomes) - 1
     while indexofgenomes != -1:
@@ -54,8 +62,17 @@ def generate_population_from_genomes(listofgenomes):
     
     
 def generate_initial_population(numberofindividuals, numberofgenes, numberofmachines):
-    """This function generates a random list of genomes that is equivalent to the number of individuals.
-    in the list, there are lists of genes (the individuals genomes) with a random number in the range between 1 and the number of machines"""
+    """This function generates a random list of chromosoms that is equivalent to the number of individuals.
+    in the list, there are chromosoms, (or in other words the individual's genes) with a random number in the range between 1 and the number of machines
+    this function is only called once per experiment, as one only needs one initial population. the initial population is then manipulated by other functions such
+    as the recombination function, the selection function or the mutation function
+    
+    INPUT: 
+    Number of individuals per population (user defined in another function)
+    Number of genes. This is equivalent to the total number of jobs to distribute
+    Number of machines. This is important to distribute the integer range of single genes. In our case, this is always 20
+    
+    OUTPUT: A list of random individuals (first population)"""
     initialgenes = []
     for individual in numberofindividuals:
         genome = []
@@ -66,7 +83,4 @@ def generate_initial_population(numberofindividuals, numberofgenes, numberofmach
         initialgenes.append(genome)
     
     return initialgenes
-
-def evalfitness(genome):
-    return 1
     
