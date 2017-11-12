fc5e96f5b8c01666d3c7347997e4b866dc5bb08d
yannicjae <33575444+yannicjae@users.noreply.github.com>
2017-11-12 13:10:25 +0100

    Update recombination.py

--- recombination.py
+++ recombination.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 08:39:05 2017
 
+@author: JoJo
+"""
+
+import random
+
+
+def onepoint(p1,p2):
+  parentlength = length(p1) - 1
+  #create children 1 and 2
+  c1 = []
+  c2 = []
+  cuttpoint = random.randint
+
+  
+  
+  return c1,c2
+
+def initialize():
+    p1=[1,2,3,4,5,6,7,8,9]
+    p2=[9,1,8,2,7,3,6,4,5]
+
+    c1,c2 = onepoint(p1,p2)
+    
+    print(c1)
