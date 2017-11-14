1cf6ad84018c5a2e4840cade5126a0317b940e1c
yannicjae <33575444+yannicjae@users.noreply.github.com>
2017-11-14 15:25:13 +0100

    Create selectionTurnament.py

---
+++ selectionTurnament.py
@@ -0,0 +1,46 @@
+def selectionTurnament(population):
+    competitors = population
+    #fitnessValues = []
+    matingpool = []
+
+    '''
+    #for each individum in the population mapp the fitnessvalue in a list
+    for individum in competitors:
+        currentfitness = individum.fitness
+        fitnessValues.append(currentfitness)
+    '''
+
+    #defines how many individuals are in the matingpool
+    sizeMatingPool = 100
+    while sizeMatingPool > 0:
+        #store population length for quick exces
+        sizeCompetitors = len(competitors)
+        #set parents to invalid values
+        p1_index = -1
+        p2_index = -1
+        #choose differnet parents untill they are not the same individuals
+        while parent1 == parent2:
+            p1_index = random.randint(0,sizePopulation-1)
+            p2_index = random.randint(0,sizePopulation-1)
+
+        p1_fit = competitors[p1_index].fitness
+        p2_fit = competitors[p2_index].fitness
+        #p1 is fitter than p2
+        if p1_fit > p2_fit:
+            #append the matingpool with the fitter parent
+            matingpool.append(competitors[p1_index])
+            #delete the winnging parent, because he is no longer a competitor
+            competitors.pop(p1_index)
+            #shring the size of the matingpool, because we have found a parent
+            sizeMatingPool -= 1
+        #p2 is fitter than p1
+        elif p1_fit < p2_fit:
+            #append the matingpool with the fitter parent
+            matingpool.append(competitors[p2_index])
+            #delete the winnging parent, because he is no longer a competitor
+            competitors.pop(p2_index)
+            #shring the size of the matingpool, because we have found a parent
+            sizeMatingPool -= 1
+        # if nothing holds we have a sting, booth are equaly fit, so we do nothing 
+
+    return matingpool
