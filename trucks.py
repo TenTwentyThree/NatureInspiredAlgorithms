6998903b02ceccf4097cb124b1327f07d5a8d6c4
MariekeP <marieke.prien@posteo.de>
2018-01-29 11:53:55 +0100

    Update trucks.py

--- trucks.py
+++ trucks.py
@@ -13,24 +13,28 @@ truck a
 	cost = 100
 	cost_per_package = 11,111
 	amount = 20
+	total_package_amount = 180
 
 truck b
 	capacity = 13
 	cost = 300
 	cost_per_package = 23,077
 	amount = 7
+	total_package_amount = 133
 
 truck c
 	capacity = 15
 	cost = 500
 	cost_per_package = 33,333
 	amount = 4
+	total_package_amount = 60
 
 truck d
 	capacity = 18
 	cost = 1000
 	cost_per_package = 55,556
 	amount = 2
+	total_package_amount = 36
   
   
 Since cost_per_package raises significantly with each bigger truck, it makes sense to first use up all the small trucks (truck a),
@@ -43,7 +47,25 @@ For example, if there are 300 packages, we can get done without truck d so we do
 
 
 ###
-
-Pseudocode
+def truck_ration():
+	if total_packages > 409:
+		print("too many packages")
+		
+	if total_packages <= 180:
+		only use truck a
+		
+	if total_packages <= 313:
+		while packages_delivered < 180:
+			use truck a
+		use truck b
+		
+	if total_packages <= 373:
+		while packages_delivered < 313:
+			use truck a and b
+		use truck c
+	else:
+		while packages_delivered < 373:
+			use truck a, b and c
+		use truck d
 
  """
